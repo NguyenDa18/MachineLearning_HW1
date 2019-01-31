0f1c281509bb961940891aaafb9892b8518bd57f
Danh Nguyen <danhnguyen267@gmail.com>
2019-01-31 01:49:28 -0800

    Modified python file

--- HW1.py
+++ HW1.py
@@ -1,18 +1,134 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import random
 import numpy as np
 import argparse
 
 # construct argument parse
 ap = argparse.ArgumentParser()
 
-dip_df = pd.read_csv("dip-har-eff.csv")
-drift_df = pd.read_csv("drift-har-eff.csv")
-set_df = pd.read_csv("set-har-eff.csv")
+# Load dataframes
+dip_df = pd.read_csv("drift-har-eff.csv")
 
-## Sigmoid Activation
-def sigmoid_activation(x):
-    # compute and return sigmoid activation val for given input val
-    return 1.0 / (1 + np.exp(-x))
+# Gather info
+dip_rows = dip_df.shape[0]
+dip_cols = dip_df.shape[1]
 
-print(drift_df.head(5))
+dip_df = dip_df.values
+dip_df = dip_df[np.arange(0, dip_rows), :]
+
+dip_X = dip_df[:,1]
+dip_Y = dip_df[:,2]
+dip_X_max = np.max(dip_X)
+dip_Y_max = np.max(dip_Y)
+
+dip_X = np.true_divide(dip_X, dip_X_max)
+dip_Y = np.true_divide(dip_Y, dip_Y_max)
+
+# Functions
+def calc_total_Error(row, col):
+    return (b1*row+b0 - col)**2
+
+def calc_b0_Error(row, col):
+    return (b1*row+b0 - col)
+
+def calc_b1_Error(row, col):
+    return (b1*row+b0 - col) * row
+
+
+# helper wrapper function for us to plug in error function to use
+def useErrorFunction(errorFunction, args):
+    return errorFunction(*args)
+    
+def mse(X,Y, errorFunction):
+    errorsList = []
+    for (row, col) in zip(X,Y):
+        val = useErrorFunction(errorFunction, (row, col))
+        errorsList.append(val)  
+    ERROR_SUM = sum(errorsList)
+    return ERROR_SUM / batchSize
+
+def adjustWeight(weight, learn, temp_error):
+    return weight - learn * temp_error
+
+def takeRandomBatch(listLen, batchSize):
+    return random.sample(range(listLen), batchSize)
+
+def calcMean(results):
+    results_sum = sum(results)
+    return results_sum / batchSize
+
+# Related to R Squared Error Loss
+def calc_predicted(b0, b1, data):
+    predicted_vals = map(lambda x: b1*x + b0, data)
+    return predicted_vals
+
+def calcSquareDiff(data):
+    mean = calcMean(data)
+    mean_diff = map(lambda actual: actual - mean, data)
+    square_mean_diff = sum(map(lambda val: val**2, mean_diff))
+    return square_mean_diff
+
+def calcRSquared(actual, predicted):
+    predicted_R_square = calcSquareDiff(predicted)
+    actual_R_square = calcSquareDiff(actual)
+    print(predicted_R_square)
+    print(actual_R_square)
+    return predicted_R_square / actual_R_square
+
+# Start of Regression
+
+# init weights
+b1 = 1.0
+b0 = -0.5
+batchSize = 10
+epochs = 100
+
+learn = 0.4
+
+plt.figure(figsize=(9,4))
+
+# Get current size
+# fig_size = plt.rcParams["figure.figsize"]
+# plt.rcParams["figure.figsize"] = [9.5,6]
+
+plt.subplot(211)
+
+plt.xlabel('Dipnet days fished')
+plt.ylabel('Dipnet days fished')
+plt.title('Dipnet fishermen')
+plt.scatter(dip_X, dip_Y)
+plt.pause(0.1);
+
+batchSample = takeRandomBatch(len(dip_X), batchSize)
+sample_X = [dip_X[i] for i in sorted(batchSample)]
+sample_Y = [dip_Y[i] for i in sorted(batchSample)]
+
+
+batch_epoch_ERRORS = []
+for i in range(epochs):
+    ERROR = mse(dip_X, dip_Y, calc_total_Error)
+    batch_epoch_ERRORS.append(ERROR)
+    b0_temp_error = mse(sample_X, sample_Y, calc_b0_Error)
+    b1_temp_error = mse(sample_X, sample_Y, calc_b1_Error)
+    b0 = adjustWeight(b0, learn, b0_temp_error)
+    b1 = adjustWeight(b1, learn, b1_temp_error)
+    print(" b0: " + str(b0) + " b1: " + str(b1) + " Error: " + str(ERROR))
+    # How to plot the graph
+    X_test = np.arange(0,1,0.1)
+    plt.plot (X_test, b1*X_test + b0)
+    plt.pause(0.1)
+    
+plt.subplot(212)
+plt.xlabel('Iteration')
+plt.ylabel('Error (MSE)')
+
+predicted = calc_predicted(b0,b1,sample_X)
+print('R-Squared: ')
+print(calcRSquared(sample_Y, predicted))
+
+# Plot for error loss
+for count, value in enumerate(batch_epoch_ERRORS):
+    plt.scatter(count,value)
+
+plt.show()
